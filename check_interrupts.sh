a7a61fb8fc7d60b4c072b18d41aa356252cf2356
Honza Hommer <honza@hommer.cz>
2019-07-25 02:43:21 +0200

    feat: option for manual max intterupts, use vmstat

--- check_interrupts.sh
+++ check_interrupts.sh
@@ -1,52 +1,58 @@
 #!/usr/bin/env bash
 
-WARNING="75%"
-CRITICAL="90%"
-
-SYSCTL="/sbin/sysctl"
-
-NCPU="$($SYSCTL -in hw.ncpu)"
-
-help() {
-  echo "Usage: $0 -i iface [ -w interrupts ] [ -c interrupts ] [ -h ]"
-  echo ""
-  echo "  -i iface       : iface to check (eg. igb.0)"
-  echo "  -w interrupts  : warning threshold (eg. 24000 or 75%)"
-  echo "  -c interrupts  : critical threshold (eg. 28800 or 90%)"
-  echo "  -h"
-  echo ""
-}
-
-while getopts :hi:w:c: option; do
-  case "${option}" in
-    i) IFACE=${OPTARG};;
-    w) WARNING=$OPTARG;;
-    c) CRITICAL=$OPTARG;;
-    h | *) help
-    exit 3;;
-  esac
-done
-
-if ! echo "$IFACE" | grep -Eq '^i(gb|x).[0-9]+$'; then
-  help
+if ! uname | grep -iqw freebsd; then
+  echo "UNKNOWN - Unsupported platform"
   exit 3
 fi
 
-MAX_INTERRUPT_RATE="$($SYSCTL -in "hw.$(echo "$IFACE" | grep -Eio '^[a-z]+').max_interrupt_rate")"
+WARNING="75%"
+CRITICAL="90%"
 
-if [ -z "$MAX_INTERRUPT_RATE" ]; then
-  echo "UNKNOWN - IFACE is not supported"
+help() {
+  echo "Usage: $0 -i iface [ -m max ] [ -w interrupts ] [ -c interrupts ] [ -h ]"
+  echo ""
+  echo "  -i iface       : iface to check (eg. igb.0 or igb0), igb and ix supported"
+  echo "  -m max         : maximum interrupts (eg. 32000), default from kernel parameters"
+  echo "  -w interrupts  : warning threshold (eg. 24000 or 75%), default 75%"
+  echo "  -c interrupts  : critical threshold (eg. 28800 or 90%), default 90%"
+  echo "  -h"
+  echo ""
+  exit 3
+}
+
+while getopts :hi:m:w:c: option; do
+  case "${option}" in
+    i) IFACE=$(echo "$OPTARG" | grep -E '^i(gb|x).?[0-9]+$' | tr -d .) ;;
+    m) MAX=${OPTARG##*[!0-9]*} ;;
+    w) WARNING=${OPTARG} ;;
+    c) CRITICAL=${OPTARG} ;;
+    h | *) help ;;
+  esac
+done
+
+if [ -z "$IFACE" ]; then
+  echo "UNKNOWN - Please specify IFACE"
+  exit 3
+elif ! ifconfig "$IFACE" >/dev/null 2>&1; then
+  echo "UNKNOWN - No IFACE ($IFACE) found"
+  exit 3
+fi
+
+if [ -z ${MAX+x} ]; then
+  MAX=$(sysctl -in "hw.$(echo "$IFACE" | grep -Eio '^[a-z]+').max_interrupt_rate")
+elif [ -z "$MAX" ]; then
+  echo "UNKNOWN - MAX ($MAX) is not number"
   exit 3
 fi
 
 if echo "$WARNING" | grep -q %\$; then
   WARNING="$(echo "$WARNING" | grep -Eo '^[0-9]+')"
-  WARNING="$((MAX_INTERRUPT_RATE*WARNING/100))"
+  WARNING="$((MAX*WARNING/100))"
 fi
 
 if echo "$CRITICAL" | grep -q %\$; then
   CRITICAL="$(echo "$CRITICAL" | grep -Eo '^[0-9]+')"
-  CRITICAL="$((MAX_INTERRUPT_RATE*CRITICAL/100))"
+  CRITICAL="$((MAX*CRITICAL/100))"
 fi
 
 if [ $WARNING -ge $CRITICAL ]; then
@@ -54,15 +60,10 @@ if [ $WARNING -ge $CRITICAL ]; then
   exit 3
 fi
 
-INTERRUPTS="$(for ((q=0; q<NCPU; q++)); do
-  for i in {1..3}; do
-    seq 10 | xargs -I{} sysctl -in "dev.$IFACE.queue$q.interrupt_rate" | sort -n | awk ' { a[i++]=$1; } END { print a[int(i/2)]; }'
-    sleep 1
-  done | sort -n | head -n1
-done | awk '{ s += $1 } END { print s }')"
+INTERRUPTS=$(vmstat -i | awk '$2 ~ /^'$IFACE':que$/ && $3 >= 0 { s += $NF } END { print s }')
 
 if [ -z "$INTERRUPTS" ]; then
-  echo "UNKNOWN - Unable to get IFACE interrupts"
+  echo "UNKNOWN - Unable to get IFACE ($IFACE) interrupts"
   exit 3
 elif [ "$INTERRUPTS" -ge $CRITICAL ]; then
   echo "CRITICAL - $INTERRUPTS interrupts"
